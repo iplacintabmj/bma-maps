9156b9129f9d003ce935093159a97ecc21d6cadd
Pedro Lopez <plopez@bmj.com>
2015-10-21 19:10:00 +0100

    Add POIs

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -27,4 +27,23 @@ angular.module('starter.controllers', ['starter.directives'])
   };
 })
 
-.controller("InterestpointsCtrl", function($scope) {});
+.controller("InterestpointsCtrl", function($scope, $http) {
+
+  var data = {
+    buid : 'building_50f56644-9101-42b0-b2d9-49df37fe95db_1445422155408'
+  };
+
+  $http.post('http://localhost:8100/anyplace/mapping/pois/all_building', data)
+    .then(function(resp) {
+      $scope.pois = resp.data.pois.filter(function(item)
+      {
+        return item.name != 'Connector';
+      });
+      console.log('Success', resp);
+      // For JSON responses, resp.data contains the result
+    }, function(err) {
+      console.error('ERR', err);
+      // err.status will contain the status code
+    })
+
+});
