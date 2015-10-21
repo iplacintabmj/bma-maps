3a3387cda039dc0c137f983ddd8ebc49d22854c4
Pedro Lopez <plopez@bmj.com>
2015-10-21 21:50:15 +0100

    Add Search and Images in the modal panel

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -27,7 +27,10 @@ angular.module('starter.controllers', ['starter.directives'])
   };
 })
 
-.controller("InterestpointsCtrl", function($scope, $http) {
+.controller("InterestpointsCtrl", function($scope, $http, $ionicModal, $ionicFilterBar) {
+  var filterBarInstance;
+
+  $scope.listCanSwipe = true;
 
   var data = {
     buid : 'building_50f56644-9101-42b0-b2d9-49df37fe95db_1445422155408'
@@ -44,6 +47,36 @@ angular.module('starter.controllers', ['starter.directives'])
     }, function(err) {
       console.error('ERR', err);
       // err.status will contain the status code
-    })
+    });
+
+  $ionicModal.fromTemplateUrl('picture-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  $scope.openModal = function(poi) {
+    console.log('Showing picture for POI', poi);
+    $scope.currentPoi = poi;
+    $scope.imageUrl = 'http://localhost:8100/images/' + poi.puid + ".jpg";
+    $scope.modal.show();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+
+  $scope.showFilterBar = function () {
+      filterBarInstance = $ionicFilterBar.show({
+        items: $scope.pois,
+        update: function (filteredItems) {
+          $scope.pois = filteredItems;
+        },
+        filterProperties: 'name'
+      });
+    };
 
 });
