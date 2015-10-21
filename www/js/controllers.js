3081c9e971093ca8d81bcddbb84b54c60675722a
Irina Placinta <iplacinta@bmj.com>
2015-10-21 18:47:45 +0100

    init

---
+++ www/js/controllers.js
@@ -0,0 +1,30 @@
+angular.module('starter.controllers', ['starter.directives'])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('MapCtrl', function($scope, $ionicLoading) {
+  $scope.mapCreated = function(map) {
+    $scope.map = map;
+  };
+
+  $scope.centerOnMe = function () {
+    console.log("Centering");
+    if (!$scope.map) {
+      return;
+    }
+
+    $scope.loading = $ionicLoading.show({
+      content: 'Getting current location...',
+      showBackdrop: false
+    });
+
+    navigator.geolocation.getCurrentPosition(function (pos) {
+      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+      $scope.loading.hide();
+    }, function (error) {
+      alert('Unable to get location: ' + error.message);
+    });
+  };
+})
+
+.controller("InterestpointsCtrl", function($scope) {});
