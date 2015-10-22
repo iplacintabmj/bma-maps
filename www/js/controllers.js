0f5c044c0a7fa081ed350fcf8426d6a563b2b935
Pedro Lopez <plopez@bmj.com>
2015-10-22 11:47:12 +0100

    Add POI to map feature

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,10 @@ angular.module('starter.controllers', ['starter.directives'])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('MapCtrl', function($scope, $ionicLoading) {
+.controller('MapCtrl', function($rootScope, $scope, $ionicLoading) {
+
+  var markers = [];
+
   $scope.mapCreated = function(map) {
     $scope.map = map;
   };
@@ -20,14 +23,32 @@ angular.module('starter.controllers', ['starter.directives'])
 
     navigator.geolocation.getCurrentPosition(function (pos) {
       $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-      $scope.loading.hide();
+      $ionicLoading.hide();
     }, function (error) {
       alert('Unable to get location: ' + error.message);
     });
   };
+
+  $rootScope.$on('loadPoi', function(event, poi) {
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(null);
+        }
+
+        $scope.map.setCenter(new google.maps.LatLng(poi.coordinates_lat, poi.coordinates_lon));
+        $scope.map.setZoom(19);
+
+        var myLatLng = {lat: +poi.coordinates_lat, lng: +poi.coordinates_lon};
+
+        var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: $scope.map,
+          title: poi.name
+        });
+        markers.push(marker);
+    });
 })
 
-.controller("InterestpointsCtrl", function($scope, $http, $ionicModal, $ionicFilterBar) {
+.controller("InterestpointsCtrl", function($rootScope, $scope, $http, $ionicModal, $ionicFilterBar, $location) {
   var filterBarInstance;
 
   $scope.listCanSwipe = true;
@@ -55,19 +76,17 @@ angular.module('starter.controllers', ['starter.directives'])
   }).then(function(modal) {
     $scope.modal = modal;
   });
+
   $scope.openModal = function(poi) {
     console.log('Showing picture for POI', poi);
     $scope.currentPoi = poi;
     $scope.imageUrl = 'http://localhost:8100/images/' + poi.puid + ".jpg";
     $scope.modal.show();
   };
+
   $scope.closeModal = function() {
     $scope.modal.hide();
   };
-  //Cleanup the modal when we're done with it!
-  $scope.$on('$destroy', function() {
-    $scope.modal.remove();
-  });
 
   $scope.showFilterBar = function () {
       filterBarInstance = $ionicFilterBar.show({
@@ -78,5 +97,16 @@ angular.module('starter.controllers', ['starter.directives'])
         filterProperties: 'name'
       });
     };
+
+  $scope.showInMap = function (poi) {
+      console.log('Calling map with coordinates ' + poi.coordinates_lat + ":" + poi.coordinates_lon);
+      $rootScope.$broadcast('loadPoi', poi);
+      $location.path("tab/map");
+  };
+
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
 
 });
